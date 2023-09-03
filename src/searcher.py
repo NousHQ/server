27b04e5472706c36fbce9fa49c1f0c58d719b7a5
Siddharth Balyan <balyan.sid@gmail.com>
2023-09-03 14:14:19 +0000

    fixed no user error

--- src/searcher.py
+++ src/searcher.py
@@ -3,9 +3,15 @@ from weaviate import Client
 from weaviate.gql.get import HybridFusion
 from weaviate.exceptions import UnexpectedStatusCodeException
 from fastapi import HTTPException, status
+from functools import lru_cache
+
+# no_schema_exception = HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="You haven't saved anything!")
+
+@lru_cache
+def get_failed_exception():
+    return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="You haven't saved anything!")
 
 def searcher(client: Client, query: str, user_id: str):
-    no_schema_exception = HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="You haven't saved anything!")
     source_class = settings.KNOWLEDGE_SOURCE_CLASS.format(user_id)
     content_class = settings.CONTENT_CLASS.format(user_id)
     query = "query: " + query
@@ -21,19 +27,23 @@ def searcher(client: Client, query: str, user_id: str):
         )
     except UnexpectedStatusCodeException as e:
         print(e)
-        raise no_schema_exception
-
+        raise get_failed_exception()
+    print(response)
     results = []
     unique_uris_titles = set()
-    for i, r in enumerate(response["data"]["Get"][content_class]):
-        uri = r["hasCategory"][0]["uri"]
-        title = r["hasCategory"][0]["title"]
-        if (uri, title) not in unique_uris_titles:
-            unique_uris_titles.add((uri, title))
-            results.append({
-                "index": i,
-                "uri": r["hasCategory"][0]["uri"],
-                "title": r["hasCategory"][0]["title"],
-            })
+    try:
+        for i, r in enumerate(response["data"]["Get"][content_class]):
+            uri = r["hasCategory"][0]["uri"]
+            title = r["hasCategory"][0]["title"]
+            if (uri, title) not in unique_uris_titles:
+                unique_uris_titles.add((uri, title))
+                results.append({
+                    "index": i,
+                    "uri": r["hasCategory"][0]["uri"],
+                    "title": r["hasCategory"][0]["title"],
+                })
 
-    return results
+        return results
+    except KeyError as e:
+        print(e)
+        raise get_failed_exception()
