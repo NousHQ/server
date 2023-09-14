d016abab6f7778bbab4c03a709ce36f3a4d00c53
Siddharth Balyan <balyan.sid@gmail.com>
2023-09-14 14:08:33 +0000

    test origin fix

--- src/main.py
+++ src/main.py
@@ -44,7 +44,7 @@ origins = [
     "https://app.nous.fyi"
 ]
 
-test_origins = "^https:\/\/nous-frontend-.*\.vercel\.app\/$"
+test_origins = "^https://nous-frontend-.*\.vercel\.app"
 
 app.add_middleware(
     CORSMiddleware,
