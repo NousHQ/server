3328e2cfdff998cf054469a6f0f64aa89984d786
Siddharth Balyan <balyan.sid@gmail.com>
2023-11-30 13:55:23 +0530

    Fix import_bookmarks function to include retry and timeout options

--- src/main.py
+++ src/main.py
@@ -233,9 +233,9 @@ async def delete_user(id: str, current_user: TokenData = Depends(get_current_use
 @app.post("/api/import")
 async def import_bookmarks(webhookData: Payload):
     from redis import Redis
-    from rq import Queue
+    from rq import Queue, Retry
 
     redis_conn = Redis(host=settings.JOBS_QUEUE, port=6379, db=0)
-    q = Queue('default', connection=redis_conn)
-    job = q.enqueue('main.importer', webhookData.model_dump())
+    q = Queue('default', connection=redis_conn, default_timeout=1200)
+    job = q.enqueue('main.importer', webhookData.model_dump(), retry=Retry(max=3, interval=60))
     logger.info(f"Job {job.id} enqueued")
